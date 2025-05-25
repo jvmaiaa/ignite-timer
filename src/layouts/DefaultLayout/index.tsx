5a911b7ffc7b0dc0811f87bda593d7296dd8de76
jvmiaa <joaovictormaia482@gmail.com>
2025-05-25 00:27:45 -0300

    ✨[feat]: Adiciona e configura DefaultLayout nas rotas e estiliza o Header.

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../components/Header';
+import { Header } from '../../components/Header';
+import { LayoutContainer } from './styles';
 
 export const DefaultLayout = () => {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet /> {/*Local que será substituído pelo conteúdo da rota */}
-    </div>
+    </LayoutContainer>
   );
 };
